9b91085761687ca829cbc0f0b65411296004f52f
ram-meegada <ramumeegada1811@gmail.com>
2025-05-09 08:17:08 +0530

    may 9 changes

---
+++ app/pages/CustomersList.tsx
@@ -0,0 +1,21 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { globalStyle } from "@/constants/globalStyles";
+
+
+const CustomersList = () => {
+    const l = [
+        
+    ]
+  return (
+    <View style={globalStyle.container}>
+      <View style={globalStyle.subContainer}>
+        <Text style={globalStyle.pageHeadingStyle}>Customers</Text>
+      </View>
+    </View>
+  );
+};
+
+export default CustomersList;
+
+const styles = StyleSheet.create({});
